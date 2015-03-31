1f72fd2f8d361fe91bbbe7c547d236bc5e1cec57
chris-bailey <chris@chrisbaileydeveloper.com>
2015-03-31 14:25:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## Sum of Two Elements ##
+Given a number and integer array find if there are two elements that sum to the target. Java solution explained in detail on my [website](http://chrisbaileydeveloper.com/projects/sum-of-two-elements/).
+
+## Steps to set up development environment in Eclipse ##
+1. **Download** or clone the project from GitHub.  
+2. Navigate to the project directory and run the following from the command line:
+  
+    `$ mvn package`
+
+3.	Open Eclipse in order to import the project.
+4.	File -> Import -> Maven -> **Existing Maven Projects** (Navigate to folder and import)  
+
+### src/main/java ###
+The **Main** class in the com.chrisbaileydeveloper.sumoftwoelements package holds the solution.
+
+### src/test/java ###
+The **MainTest** class in the com.chrisbaileydeveloper.sumoftwoelements package holds the JUnit test cases.
+
+### Development environment ###
+Windows 7 64bit, Eclipse IDE STS Luna 4.4.1, Maven 3.1.0
+
+### Author ###
+[Chris Bailey](http://www.chrisbaileydeveloper.com)
---
+++ src/test/java/com/chrisbaileydeveloper/sumoftwoelements/MainTest.java
@@ -0,0 +1,72 @@
+package com.chrisbaileydeveloper.sumoftwoelements;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+/**
+ * JUnit testing of com.chrisbaileydeveloper.sumoftwoelements.Main class
+ * 
+ * @author Chris Bailey
+ */
+public class MainTest {
+
+	static int[] mainArray = { 5, 2, 7, 1, 3, 9, 2, 7, 3, 2 };
+	static int[] nullArray = null;
+	static int target = 8;
+
+	static List<int[]> expectedList = new ArrayList<>();
+	static List<int[]> resultList = new ArrayList<>();
+
+	static Object[] expectedArray;
+	static Object[] resultArray;
+
+	@Test
+	public void testFindPairs() {
+		// Test for null array
+		expectedList = Arrays.asList(new int[] { -1, -1 });
+		resultList = Main.findPairs(nullArray, target);
+
+		expectedArray = expectedList.toArray();
+		resultArray = resultList.toArray();
+
+		assertArrayEquals(expectedArray, resultArray);
+
+		
+		// Test normal operation
+		expectedList = Arrays.asList(new int[] { 0, 4 }, new int[] { 0, 8 },
+									 new int[] { 2, 3 }, new int[] { 3, 7 });
+		resultList = Main.findPairs(mainArray, target);
+
+		expectedArray = expectedList.toArray();
+		resultArray = resultList.toArray();
+
+		assertArrayEquals(expectedArray, resultArray);
+	}
+
+	@Test
+	public void testFindPairsSimply() {
+		// Test for null array
+		expectedList = Arrays.asList(new int[] { -1, -1 });
+		resultList = Main.findPairsSimply(nullArray, target);
+
+		expectedArray = expectedList.toArray();
+		resultArray = resultList.toArray();
+
+		assertArrayEquals(expectedArray, resultArray);
+
+		
+		// Test normal operation
+		expectedList = Arrays.asList(new int[] { 0, 4 }, new int[] { 0, 8 },
+									 new int[] { 2, 3 }, new int[] { 3, 7 });
+		resultList = Main.findPairsSimply(mainArray, target);
+
+		expectedArray = expectedList.toArray();
+		resultArray = resultList.toArray();
+
+		assertArrayEquals(expectedArray, resultArray);
+	}
+}
